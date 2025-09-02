5a84f3f574add2dbbe5c751b57808fb6a50b3674
Sadman Jahin <sadman.jahin@vml.com>
2025-09-02 18:47:10 +0600

    swagger added

--- BackendProject/BackendProject/Program.cs
+++ BackendProject/BackendProject/Program.cs
@@ -6,7 +6,11 @@ builder.AddServiceDefaults();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi();
+
+#region Swagger
+builder.Services.AddSwaggerGen();
+#endregion
 
 var app = builder.Build();
 
@@ -15,7 +19,9 @@ app.MapDefaultEndpoints();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
