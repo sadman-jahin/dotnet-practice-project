a1f81813415463e005caf42c8de1e8fba40f0307
Sadman Jahin <sadman.jahin@vml.com>
2025-09-09 15:05:19 +0600

    adding resilence

--- BackendProject/ApiClient.Application/Services/HttpApiClient.cs
+++ BackendProject/ApiClient.Application/Services/HttpApiClient.cs
@@ -1,9 +1,8 @@
 ﻿using ApiClient.Application.Interfaces;
+using Polly;
+using Polly.Retry;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -12,13 +11,21 @@ namespace ApiClient.Application.Services
     public class HttpApiClient : IApiClient
     {
         private readonly HttpClient _httpClient;
+        private readonly AsyncRetryPolicy _retryPolicy;
+
         public HttpApiClient(HttpClient httpClient)
         {
             _httpClient = httpClient;
+
+            _retryPolicy = Policy
+                .Handle<HttpRequestException>()
+                .WaitAndRetryAsync(3, attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)));
         }
+
         public async Task<T> GetDataAsync<T>(string url)
         {
-            var response = await _httpClient.GetAsync(url);
+            var response = await _retryPolicy.ExecuteAsync(() => _httpClient.GetAsync(url));
+
             response.EnsureSuccessStatusCode(); // Throws if not 2xx
 
             string content = await response.Content.ReadAsStringAsync();
